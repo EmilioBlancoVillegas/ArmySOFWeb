bc0b7931323cc6581cd3936bbf3f1c5a1a0090d3
Emilio Blanco Villegas <eblanco@cic.es>
2018-12-18 15:28:49 +0100

    First Commit

---
+++ es.ArmySOF.WebSite/es.ArmySOF.WebSite/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using es.ArmySOF.WebSite.Classes;
+using es.ArmySOF.WebSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace es.ArmySOF.WebSite.Controllers
+{
+	[AllowAnonymous]
+	[RoutePrefix("api/login")]
+    public class LoginController : ApiController
+    {
+		[HttpGet]
+		[Route("echoping")]
+		public IHttpActionResult EchoPing()
+		{
+			return Ok(true);
+		}
+
+		[HttpGet]
+		[Route("echouser")]
+		public IHttpActionResult EchoUser()
+		{
+			var identity = Thread.CurrentPrincipal.Identity;
+			return Ok($" IPrincipal-user: {identity.Name} - IsAuthenticated: {identity.IsAuthenticated}");
+		}
+
+		[HttpPost]
+		[Route("authenticate")]
+		public IHttpActionResult Authenticate(LoginRequest login)
+		{
+			if (login == null)
+				throw new HttpResponseException(HttpStatusCode.BadRequest);
+			bool isValid = login.Password == "zz123";
+			if (isValid)
+			{
+				var token = TokenGenerator.GenerateTokenJwt(login.UserName);
+				return Ok(token);
+			}
+			else
+				return Unauthorized();
+		}
+    }
+}
---
+++ es.ArmySOF.WebSite/es.ArmySOF.WebSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="es.ArmySOF.WebSite.WebApiApplication" Language="C#" %>
